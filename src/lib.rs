50336b98e1b2c551cf824e1771fed562c489a2e3
Talin <viridia@gmail.com>
2024-11-04 17:28:29 -0800

    WithEffect

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ mod children;
 mod cond;
 mod effect;
 mod switch;
+mod with_effect;
 
 pub use children::{ChildTuple, WithChildren};
 pub use cond::Cond;
 pub use effect::{EffectCell, EffectPlugin};
 pub use switch::Switch;
+pub use with_effect::WithEffect;
